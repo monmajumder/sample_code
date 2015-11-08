f637d8d988f75375d27b042e213ed7e24420b154
Jenny Wagner <jennywagg@gmail.com>
2015-11-08 14:40:08 -0500

    Game room name added programmatically

--- TheResistance/app/src/main/java/com/resistance/theresistance/activities/GameActivity.java
+++ TheResistance/app/src/main/java/com/resistance/theresistance/activities/GameActivity.java
@@ -2,12 +2,8 @@ package com.resistance.theresistance.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.widget.TextView;
+import android.widget.RelativeLayout;
 
 import com.resistance.theresistance.R;
 
@@ -23,14 +19,23 @@ public class GameActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
         // Get the message from the intent
         Intent intent = getIntent();
+
+        String gameRoomStr = this.getResources().getString(R.string.game_room_name);
         String gameName = intent.getStringExtra(GameNameActivity.EXTRA_MESSAGE);
+        gameRoomStr = gameRoomStr + " " + gameName;
+
+        ((com.resistance.theresistance.views.MyTextView)findViewById(R.id.game_room_name)).setText(gameRoomStr);
 
         // Create the text view
-        TextView textView = new TextView(this);
-        textView.setTextSize(40);
-        textView.setText(gameName);
-        setContentView(textView);
+//        com.resistance.theresistance.views.MyTextView gameNameView = new com.resistance.theresistance.views.MyTextView(this);
+//        gameNameView.setTextSize(40);
+//        gameNameView.setText(gameName);
+        //gameNameView.setLayoutParams();
+
+        RelativeLayout relativeLayout = (RelativeLayout)findViewById(R.id.content_game);
+
     }
 }
