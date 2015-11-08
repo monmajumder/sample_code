0c67a811c91916c619f30387be899137081061fd
Jenny Wagner <jennywagg@gmail.com>
2015-11-08 17:05:03 -0500

    Starting some tab stuff

--- TheResistance/app/src/main/java/com/resistance/theresistance/activities/GameActivity.java
+++ TheResistance/app/src/main/java/com/resistance/theresistance/activities/GameActivity.java
@@ -3,7 +3,8 @@ package com.resistance.theresistance.activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.RelativeLayout;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.resistance.theresistance.R;
 
@@ -19,23 +20,20 @@ public class GameActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+
+
         setContentView(R.layout.activity_game);
         // Get the message from the intent
         Intent intent = getIntent();
-
-        String gameRoomStr = this.getResources().getString(R.string.game_room_name);
         String gameName = intent.getStringExtra(GameNameActivity.EXTRA_MESSAGE);
+        String gameRoomStr = this.getResources().getString(R.string.game_room_name);
+
         gameRoomStr = gameRoomStr + " " + gameName;
 
         ((com.resistance.theresistance.views.MyTextView)findViewById(R.id.game_room_name)).setText(gameRoomStr);
 
-        // Create the text view
-//        com.resistance.theresistance.views.MyTextView gameNameView = new com.resistance.theresistance.views.MyTextView(this);
-//        gameNameView.setTextSize(40);
-//        gameNameView.setText(gameName);
-        //gameNameView.setLayoutParams();
-
-        RelativeLayout relativeLayout = (RelativeLayout)findViewById(R.id.content_game);
-
+        ImageView player1 = new ImageView(this);
+        player1.setVisibility(View.VISIBLE);
     }
 }
