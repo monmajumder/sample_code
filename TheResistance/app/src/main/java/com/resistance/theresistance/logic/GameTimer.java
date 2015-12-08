da95c525087799970f9f38151c3fcfe7cc09be1a
cliu72 <candace.liu94@gmail.com>
2015-12-08 10:52:34 -0500

    Add timer for checking if a game has started

--- TheResistance/app/src/main/java/com/resistance/theresistance/logic/GameTimer.java
+++ TheResistance/app/src/main/java/com/resistance/theresistance/logic/GameTimer.java
@@ -1,5 +1,6 @@
 package com.resistance.theresistance.logic;
 
+import android.content.Intent;
 import android.util.Log;
 import android.view.View;
 
@@ -7,22 +8,35 @@ import com.resistance.theresistance.R;
 import com.resistance.theresistance.activities.GameNameActivity;
 import com.resistance.theresistance.activities.GamePlayActivity;
 import com.resistance.theresistance.activities.GameWaitingActivity;
+import com.resistance.theresistance.views.MyTextView;
 
+import java.util.ArrayList;
 import java.util.Timer;
 import java.util.TimerTask;
 
 /**
- * Created by Candace on 12/7/2015.
+ * Handles all tasks that are run on a timer. i.e. tasks that periodically pull from Parse until there is a change in state.
  */
 public class GameTimer {
 
+    public final static String EXTRA_MESSAGE = "com.resistance.theresistance.MESSAGE";
+    private static GameWaitingActivity activity;
+
     /**
-     * Runs the Timer task for checking when a Mission Leader has finished choosing the team.
+     * Constructor
+     * @param context to access the application
+     */
+    public GameTimer(GameWaitingActivity context) {
+        this.activity = context;
+    }
+
+    /**
+     * Runs the Timer task for checking if a game has started.
+     * @param activity GameWaitingActivity
      * @param gameName name of the game
      */
-    public static void missionLeaderDoneChoosing(GamePlayActivity activity, String gameName) {
-        final GamePlayActivity thisActivity = activity;
-        final GameController timerController = new GameController();
+    public static void gameStarted(GameWaitingActivity activity, String gameName) {
+        final GameWaitingActivity thisActivity = activity;
         final String game = gameName;
         final Timer timer = new Timer();
         timer.schedule(new TimerTask() {
@@ -31,12 +45,64 @@ public class GameTimer {
                 thisActivity.runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
-                        if (timerController.checkMissionLeaderDoneChoosing(game)) {
+                        if (!GameController.checkStarted(game)) {
+                            ArrayList<String> players = GameController.updatePlayers(game);
+                            if (players == null) {
+                                Log.d("Players array", "IS NULL");
+                            }
+                            setPlayerNames(thisActivity, players);
+                            Log.d("TIMER CHECK", "GAME HAS NOT STARTED");
+                        } else {
+                            timer.cancel();
+                            startGamePlayActivity(game);
+                            Log.d("TIMER CHECK", "GAME HAS STARTED");
+                        }
+                    }
+                });
+            }
+        }, 0, 1000);
+    }
+
+    /**
+     * Updates the game waiting UI with player names who have joined.
+     * @param activity GameWaitingActivity
+     * @param players ArrayList of player names
+     */
+    private static void setPlayerNames(GameWaitingActivity activity, ArrayList<String> players){
+        MyTextView player1 = (MyTextView)activity.findViewById(R.id.pink_player_label);
+        player1.setText(players.get(0));
+    }
+
+    /**
+     * Starts the new activity after the game has started.
+     */
+    private static void startGamePlayActivity(String gameName) {
+        Intent intent = new Intent(GameWaitingActivity.getContext(), GamePlayActivity.class);
+        intent.putExtra(EXTRA_MESSAGE, gameName);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        GameWaitingActivity.getContext().startActivity(intent);
+    }
+
+    /**
+     * Runs the Timer task for checking when a Mission Leader has finished choosing the team.
+     * @param gameName name of the game
+     */
+    public static void missionLeaderDoneChoosing(GamePlayActivity activity, String gameName) {
+        final GamePlayActivity thisActivity = activity;
+        final String game = gameName;
+        final Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                thisActivity.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        if (GameController.checkMissionLeaderDoneChoosing(game)) {
                             timer.cancel();
                             changeView(thisActivity);
-                            // Log.d("TIMER CHECK", "I AM DONE");
+                            Log.d("TIMER CHECK", "I AM DONE");
                         } else {
-                            // Log.d("TIMER CHECK", "ONE TIME");
+                            Log.d("TIMER CHECK", "ONE TIME");
                         }
                     }
                 });
