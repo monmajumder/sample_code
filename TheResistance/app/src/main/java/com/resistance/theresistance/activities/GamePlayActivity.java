c45bbb05afd960ebe8e7a65d12efc09bc63abc43
Monica <mmajumder94@gmail.com>
2015-12-10 21:12:18 -0500

    End Game Activity Rough Feature

--- TheResistance/app/src/main/java/com/resistance/theresistance/activities/GamePlayActivity.java
+++ TheResistance/app/src/main/java/com/resistance/theresistance/activities/GamePlayActivity.java
@@ -13,8 +13,6 @@ import android.util.Log;
 import com.resistance.theresistance.R;
 import com.resistance.theresistance.logic.Game;
 import com.resistance.theresistance.logic.GameController;
-import com.resistance.theresistance.logic.GameTimer;
-import com.resistance.theresistance.logic.Round;
 import com.resistance.theresistance.views.GamePlayFragment;
 
 import java.util.ArrayList;
@@ -188,13 +186,12 @@ public class GamePlayActivity extends FragmentActivity {
     public void testGame() {
         //TEST IF ISRESISTANCE WORKS. DELETE.
         SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
-        String storedPlayer = preferences.getString("playerName","none");
+        String storedPlayer = preferences.getString("playerName", "none");
         Log.d("checkPlayerName", storedPlayer);
         if (GameController.isResistance(storedPlayer)) {
             Log.d("IsResistance", storedPlayer);
         } else {
-            Log.d("IsNotResistance",storedPlayer);
+            Log.d("IsNotResistance", storedPlayer);
         }
     }
-
 }
